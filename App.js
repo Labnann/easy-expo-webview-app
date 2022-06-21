b6064ef2788c3e4eaecbc66760717714deaad9fa
Khalid Masum <khalid.masum.92@gmail.com>
2022-06-22 01:01:58 +0600

    start yarn
    Signed-off-by: Khalid Masum <khalid.masum.92@gmail.com>

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {WebView} from 'react-native-webview';
+import {uri} from './uri.js'
 
 export default function App() {
   return (
-    <WebView source={{uri: 'https://www.google.com'}}/>  );
+    <WebView source={{uri}}/>  );
 }
 
 const styles = StyleSheet.create({
